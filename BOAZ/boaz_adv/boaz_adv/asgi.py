4a63235e98df762a3df6498076c70b298becab65
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-01-19 14:43:11 +0900

    Add files via upload

---
+++ BOAZ/boaz_adv/boaz_adv/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for boaz_adv project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boaz_adv.settings')
+
+application = get_asgi_application()
