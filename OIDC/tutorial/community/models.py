0a771c383a64a3bc9738d7cebbc8ba71438fa853
skm0626 <skm0626@kookmin.ac.kr>
2022-07-05 22:49:11 +0900

    oidc tutorial

---
+++ OIDC/tutorial/community/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+# 모델은 기본적으로 class로 구성됨
+class Article(models.Model):
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    contents = models.TextField()
+    url = models.URLField()
+    email = models.EmailField()
+    cdate = models.DateTimeField(auto_now_add=True)
